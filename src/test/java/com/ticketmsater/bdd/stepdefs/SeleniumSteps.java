c7d2a1891bb08bed98736372a045263fbdecf410
Eric Lee <ericlee@livenation.com>
2014-08-28 21:14:28 -0700

    Adding screenshots on error

--- src/test/java/com/ticketmsater/bdd/stepdefs/SeleniumSteps.java
+++ src/test/java/com/ticketmsater/bdd/stepdefs/SeleniumSteps.java
@@ -19,58 +19,60 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class SeleniumSteps {
-	private List<byte[]> screenGrabs = new ArrayList<byte[]>();
-	private WebDriver driver;
-	private String website;
-	
-	@Given("^that I have loaded \"([^\"]*)\" in a \"([^\"]*)\"$")
-	public void that_I_have_loaded_in_a(String website, String browser) throws Throwable {
-		WebDriver driver = null;
-		this.website = website;
-		if(browser.toLowerCase().equals("firefox")){
-			driver = GridFactory.getFirefoxInstance();
-		}else if(browser.toLowerCase().equals("chrome")){
-			driver = GridFactory.getChromeInstance();
-		}else if(browser.toLowerCase().equals("phantomjs")){
-			driver = GridFactory.getPhantomJSInstance();
-		}else if(browser.toLowerCase().equals("internet explorer")){
-			driver = GridFactory.getInternetExplorerInstance();
-		}
-		this.driver = new Augmenter().augment(driver);
-	}
-	
-	@When("^I load a page")
-	public void search_for_the_term() throws Exception {
-		this.driver.get("http://" + website);
-	}
+  private List<byte[]> screenGrabs = new ArrayList<byte[]>();
+  private WebDriver driver;
+  private String website;
 
-	@Then("^search for the term \"([^\"]*)\"$")
-	public void search_for_the_term(String arg1) throws Exception {
+  @Given("^that I have loaded \"([^\"]*)\" in a \"([^\"]*)\"$")
+  public void that_I_have_loaded_in_a(String website, String browser) throws Throwable {
+    WebDriver driver = null;
+    this.website = website;
+    if (browser.toLowerCase().equals("firefox")) {
+      driver = GridFactory.getFirefoxInstance();
+    } else if (browser.toLowerCase().equals("chrome")) {
+      driver = GridFactory.getChromeInstance();
+    } else if (browser.toLowerCase().equals("phantomjs")) {
+      driver = GridFactory.getPhantomJSInstance();
+    } else if (browser.toLowerCase().equals("internet explorer")) {
+      driver = GridFactory.getInternetExplorerInstance();
+    }
+    this.driver = new Augmenter().augment(driver);
+  }
 
-		this.driver.get("http://" + website);
+  @When("^I load a page")
+  public void search_for_the_term() throws Exception {
+    try {
+      this.driver.get("http://" + website);
+    } catch (Exception e) {
+      byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      screenGrabs.add(screenshot);
+    }
+  }
 
-		WebElement element = driver.findElement(By.name("q"));
-		for(char c : arg1.toCharArray()){
-			element.sendKeys(String.valueOf(c));
-		}
-		element.submit();
-		Thread.sleep(2000);
-		byte[] screenshot = ((TakesScreenshot) driver)
-				.getScreenshotAs(OutputType.BYTES);
-		screenGrabs.add(screenshot);
-	}
+  @Then("^search for the term \"([^\"]*)\"$")
+  public void search_for_the_term(String arg1) throws Exception {
+    try {
+      this.driver.get("http://" + website);
 
-	@Then("^I should get a page that looks like this$")
-	public void I_should_get_a_page_that_looks_like_this() {
-		driver.close();
-		driver.quit();
-	}
-	
-	@After
-	public void embedScreenshot(Scenario scenario) {
-	    for(byte[] screenshot : screenGrabs){
-	        scenario.embed(screenshot, "image/png");
-	    }
-	    this.driver.close();
-	}
+      WebElement element = driver.findElement(By.name("q"));
+      for (char c : arg1.toCharArray()) {
+        element.sendKeys(String.valueOf(c));
+      }
+      element.submit();
+      Thread.sleep(2000);
+      byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      screenGrabs.add(screenshot);
+    } catch (Exception e) {
+      byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+      screenGrabs.add(screenshot);
+    }
+  }
+
+  @After
+  public void embedScreenshot(Scenario scenario) {
+    for (byte[] screenshot : screenGrabs) {
+      scenario.embed(screenshot, "image/png");
+    }
+    this.driver.close();
+  }
 }
