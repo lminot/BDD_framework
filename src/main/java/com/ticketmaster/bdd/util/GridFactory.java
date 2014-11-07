79867d3876a8188e207a3f4d19c2fc802a2121c2
Danny Paradis <Danny.Paradis@CANQCDPA-L.LYV.LiveNation.com>
2014-11-07 11:36:37 -0500

    add 2 signature methods to implement with selenium-commons: public WebDriver getFirefoxInstance(FirefoxProfile pProfile) throws Exception public WebDriver getChromeInstance(ChromeOptions pOtions) throws Exception

--- src/main/java/com/ticketmaster/bdd/util/GridFactory.java
+++ src/main/java/com/ticketmaster/bdd/util/GridFactory.java
@@ -15,108 +15,173 @@ import org.eclipse.jetty.util.log.Logger;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class GridFactory {
+public class GridFactory
+{
 
-  private Logger logger = Log.getLogger(GridFactory.class);
-  private static final String HUB_URL_ALPHA = "http://10.1.200.233:4444/wd/hub";
-  private static final String HUB_URL_BETA = "http://10.1.210.77:4444/wd/hub";
-  private static final Integer TIMEOUT_SECONDS = 120;
+	private Logger logger = Log.getLogger(GridFactory.class);
+	private static final String HUB_URL_ALPHA = "http://10.1.200.233:4444/wd/hub";
+	private static final String HUB_URL_BETA = "http://10.1.210.77:4444/wd/hub";
+	private static final Integer TIMEOUT_SECONDS = 120;
 
-  public GridFactory() {}
+	public GridFactory()
+	{
+	}
 
-  private WebDriver getBrowser(DesiredCapabilities capability) {
-    WebDriver driver = null;
-    ExecutorService executor = Executors.newCachedThreadPool();
-    String hubUrl = HUB_URL_BETA;
-    Callable<Object> task = new BrowserCreate(capability, hubUrl);
-    Future<Object> future = executor.submit(task);
+	private WebDriver getBrowser(DesiredCapabilities capability)
+	{
+		WebDriver driver = null;
+		ExecutorService executor = Executors.newCachedThreadPool();
+		String hubUrl = HUB_URL_BETA;
+		Callable<Object> task = new BrowserCreate(capability, hubUrl);
+		Future<Object> future = executor.submit(task);
 
-    try {
-      driver = (WebDriver) future.get(GridFactory.TIMEOUT_SECONDS, TimeUnit.SECONDS);
-    } catch (TimeoutException ex) {
-      logger.warn("Timed out");
-    } catch (InterruptedException e) {
-      logger.warn(e.getMessage());
-    } catch (ExecutionException e) {
-      logger.warn(e.getMessage());
-    }
+		try
+		{
+			driver = (WebDriver) future.get(GridFactory.TIMEOUT_SECONDS, TimeUnit.SECONDS);
+		}
+		catch (TimeoutException ex)
+		{
+			logger.warn("Timed out");
+		}
+		catch (InterruptedException e)
+		{
+			logger.warn(e.getMessage());
+		}
+		catch (ExecutionException e)
+		{
+			logger.warn(e.getMessage());
+		}
 
-    if (driver == null) {
-      logger.info("Browser is null, switch to backup Grid. Alerting team.");
-      hubUrl = HUB_URL_ALPHA;
-      task = new BrowserCreate(capability, hubUrl);
-      future = executor.submit(task);
+		if (driver == null)
+		{
+			logger.info("Browser is null, switch to backup Grid. Alerting team.");
+			hubUrl = HUB_URL_ALPHA;
+			task = new BrowserCreate(capability, hubUrl);
+			future = executor.submit(task);
 
-      try {
-        driver = (WebDriver) future.get(GridFactory.TIMEOUT_SECONDS, TimeUnit.SECONDS);
-      } catch (TimeoutException ex) {
-        logger.warn("Timed out");
-      } catch (InterruptedException e) {
-        logger.warn(e.getMessage());
-      } catch (ExecutionException e) {
-        logger.warn(e.getMessage());
-      }
-    }
-    driver.manage().window().maximize();
-    //driver = new Augmenter().augment(driver);
-    return driver;
-  }
+			try
+			{
+				driver = (WebDriver) future.get(GridFactory.TIMEOUT_SECONDS, TimeUnit.SECONDS);
+			}
+			catch (TimeoutException ex)
+			{
+				logger.warn("Timed out");
+			}
+			catch (InterruptedException e)
+			{
+				logger.warn(e.getMessage());
+			}
+			catch (ExecutionException e)
+			{
+				logger.warn(e.getMessage());
+			}
+		}
+		driver.manage().window().maximize();
+		// driver = new Augmenter().augment(driver);
+		return driver;
+	}
 
-  public WebDriver getInternetExplorerInstance() throws Exception {
+	public WebDriver getInternetExplorerInstance() throws Exception
+	{
 
-    DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
-    capability.setCapability("takeScreenshot", true);
-    capability.setPlatform(Platform.WINDOWS);
-    capability.setBrowserName("internet explorer");
+		DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
+		capability.setCapability("takeScreenshot", true);
+		capability.setPlatform(Platform.WINDOWS);
+		capability.setBrowserName("internet explorer");
 
-    capability.setCapability(
-        InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-    capability.setCapability("takeScreenshot", true);
+		capability.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		capability.setCapability("takeScreenshot", true);
 
-    return getBrowser(capability);
-  }
+		return getBrowser(capability);
+	}
 
-  public WebDriver getFirefoxInstance() throws Exception {
+	public WebDriver getFirefoxInstance() throws Exception
+	{
 
-    DesiredCapabilities capability = DesiredCapabilities.firefox();
-    capability.setCapability("takeScreenshot", true);
-    return getBrowser(capability);
+		return getFirefoxInstance(null);
 
-  }
+	}
+	
+	/**
+	 * Return a WebDriver for FireFox with pProfile
+	 * If the pProfile is null set the capability with a new empty Profile
+	 * @param pProfile (FirefoxProfile)
+	 * @return (WebDriver)
+	 * @throws Exception
+	 * @author Danny.Paradis
+	 */
+	public WebDriver getFirefoxInstance(FirefoxProfile pProfile) throws Exception
+	{
+		FirefoxProfile profile = new FirefoxProfile();
+		if (pProfile != null)
+		{
+			profile = pProfile;
+		}
+		DesiredCapabilities capability = DesiredCapabilities.firefox();
+		capability.setCapability("takeScreenshot", true);
+		capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true); //TODO DPA move to default capability
+		capability.setCapability(FirefoxDriver.PROFILE, profile);
+		return getBrowser(capability);
 
-  public WebDriver getChromeInstance() throws Exception {
+	}
 
-    DesiredCapabilities capability = DesiredCapabilities.chrome();
+	public WebDriver getChromeInstance() throws Exception
+	{
+		return getChromeInstance(null);
+	}
 
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--start-maximized");
+	/**
+	 * Return a WebDriver for Chrome with pOption
+	 * If pOtions is null set the capability with a new empty Options
+	 * @param pOtions (ChromeOptions)
+	 * @return (WebDriver)
+	 * @throws Exception
+	 * @author Danny.Paradis
+	 */
+	public WebDriver getChromeInstance(ChromeOptions pOtions) throws Exception
+	{
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		ChromeOptions options = new ChromeOptions();
+		if (pOtions != null)
+		{
+			options = pOtions;
+		}
+				
+		options.addArguments("--start-maximized");
 
-    capability.setCapability(ChromeOptions.CAPABILITY, options);
-    capability.setCapability("takeScreenshot", true);
-    return getBrowser(capability);
-  }
-  
-  public WebDriver getPhantomJSInstance() throws Exception {
-    DesiredCapabilities capability = DesiredCapabilities.phantomjs();
-    return getBrowser(capability);
-  }
+		capability.setCapability(ChromeOptions.CAPABILITY, options);
+		capability.setCapability("takeScreenshot", true);
+		return getBrowser(capability);
+	}
 
-  public class BrowserCreate implements Callable<Object> {
-    private final DesiredCapabilities capability;
-    private final String hubUrl;
+	public WebDriver getPhantomJSInstance() throws Exception
+	{
+		DesiredCapabilities capability = DesiredCapabilities.phantomjs();
+		return getBrowser(capability);
+	}
 
-    public BrowserCreate(DesiredCapabilities capability, String hubUrl) {
-      this.capability = capability;
-      this.hubUrl = hubUrl;
-    }
+	public class BrowserCreate implements Callable<Object>
+	{
+		private final DesiredCapabilities capability;
+		private final String hubUrl;
 
-    public Object call() throws MalformedURLException {
-      return new RemoteWebDriver(new URL(hubUrl), capability);
-    }
+		public BrowserCreate(DesiredCapabilities capability, String hubUrl)
+		{
+			this.capability = capability;
+			this.hubUrl = hubUrl;
+		}
 
-  }
+		public Object call() throws MalformedURLException
+		{
+			return new RemoteWebDriver(new URL(hubUrl), capability);
+		}
+
+	}
 }
